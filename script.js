7c769c95a5b165b71b15d9b5df8ddde9e5efdd87
cesar-infante <cesar261110@gmail.com>
2022-05-30 20:23:37 -0700

    Comments to HTML, CSS, and JavaScript; Projects evenly aligned along with adding figcaptions, footer contact me filled in, JavaScript code sections commented out until fix for 'a href' color preventing to be white in dark mode

---
+++ script.js
@@ -0,0 +1,13 @@
+// commenting out until find solution to make links color white on dark mode.
+// const toggle = document.getElementById('toggle');
+// const body = document.querySelector('body');
+// const header = document.querySelector('header');
+// const footer = document.querySelector('footer');
+// const a = document.querySelectorAll('a');
+// toggle.onclick = function(){
+//     toggle.classList.toggle('active');
+//     body.classList.toggle('active');
+//     header.classList.toggle('active');
+//     footer.classList.toggle('active');
+//     a.classList.toggle('active');
+// }
