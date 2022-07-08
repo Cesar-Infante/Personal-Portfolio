562a14035cedbe30f7de0368721e1e2c9a23c8d7
cesar-infante <cesar261110@gmail.com>
2022-07-07 23:38:34 -0700

    Update to fix dark mode, new completed projects, Main project made the star of the show

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 // commenting out until find solution to make links color white on dark mode.
-// const toggle = document.getElementById('toggle');
-// const body = document.querySelector('body');
-// const header = document.querySelector('header');
-// const footer = document.querySelector('footer');
-// const a = document.querySelectorAll('a');
-// toggle.onclick = function(){
-//     toggle.classList.toggle('active');
-//     body.classList.toggle('active');
-//     header.classList.toggle('active');
-//     footer.classList.toggle('active');
-//     a.classList.toggle('active');
-// }
+const toggle = document.getElementById('toggle');
+var body = document.querySelector('body');
+var header = document.querySelector('header');
+var footer = document.querySelector('footer');
+var nameTitle = document.querySelector('h1')
+var navLink1 = document.querySelector('#Link1')
+var navLink2 = document.querySelector('#Link2')
+var navLink3 = document.querySelector('#Link3')
+var navLink4 = document.querySelector('#Link4')
+
+toggle.addEventListener('click', function () {
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+    header.classList.toggle('active');
+    footer.classList.toggle('active');
+    nameTitle.classList.toggle('active');
+    navLink1.classList.toggle('active');
+    navLink2.classList.toggle('active');
+    navLink3.classList.toggle('active');
+    navLink4.classList.toggle('active');
+});
+
+
