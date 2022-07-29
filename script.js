1fcb5ff8fbd9b5677c3a133b5c465216a199e8d1
Cesar's PC <cesar261110@gmaill.com>
2022-07-29 11:46:01 -0700

    update to how cards appear on scroll

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // commenting out until find solution to make links color white on dark mode.
 const toggle = document.getElementById('toggle');
-var body = document.querySelector('body');
-var header = document.querySelector('header');
-var footer = document.querySelector('footer');
-var nameTitle = document.querySelector('h1');
-var navLink1 = document.querySelector('#Link1');
-var navLink2 = document.querySelector('#Link2');
-var navLink3 = document.querySelector('#Link3');
-var navLink4 = document.querySelector('#Link4');
+let body = document.querySelector('body');
+let header = document.querySelector('header');
+let footer = document.querySelector('footer');
+let nameTitle = document.querySelector('h1');
+let navLink1 = document.querySelector('#Link1');
+let navLink2 = document.querySelector('#Link2');
+let navLink3 = document.querySelector('#Link3');
+let navLink4 = document.querySelector('#Link4');
 
 toggle.addEventListener('click', function () {
     toggle.classList.toggle('active');
@@ -20,5 +20,25 @@ toggle.addEventListener('click', function () {
     navLink3.classList.toggle('active');
     navLink4.classList.toggle('active');
 });
+
+
+
+function reveal() {
+    let revealCards = document.querySelectorAll(".reveal");
+  
+    for (let i = 0; i < revealCards.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = revealCards[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        revealCards[i].classList.add("active");
+      } else {
+        revealCards[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
 
 
